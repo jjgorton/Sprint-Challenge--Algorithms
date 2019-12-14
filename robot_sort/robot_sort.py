1c30d27a47ae4b5feb5e97553be70dac67717801
jjgorton <jjgorton@gmail.com>
2019-12-13 22:47:37 -0500

    FINALLY have all tests passing

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -102,28 +102,16 @@ class SortingRobot:
 
         self.set_light_on()
 
-        # while self.light_is_on():
-        #     if self.can_move_left():
-        #         self.move_left()
-        #         self.swap_item()
-
-        #     self.set_light_off()
-
         while self.light_is_on():
-            print(l)
+            # print('106', l)
             self.set_light_off()
-            self.swap_item()
-            if self.can_move_right():
+            while self.can_move_left():
+                self.move_left()
+            while self.can_move_right():
+                # print('113', l)
+                self.swap_item()
                 self.move_right()
-                if self.compare_item() == None:
-                    self.swap_item()
-                    if self.can_move_right():
-                        self.move_right()
-                    else:
-                        self.swap_item()
-                        while self.can_move_left():
-                            self.move_left()
-                            continue
+
                 if self.compare_item() != None and self.compare_item() > 0:
                     self.swap_item()
                     self.move_left()
@@ -132,54 +120,21 @@ class SortingRobot:
                     self.set_light_on()
                 else:
                     self.move_left()
-                    self.set_light_on()
-            else:
-                self.swap_item()
-                while self.can_move_left():
-                    self.move_left()
-                    self.set_light_on()
+                    self.swap_item()
+                    self.move_right()
 
+        # print('129', l)
         return 'Done'
-
-    # # (0) start by tunring light on
-    # set_light_on()
-
-    # # (1) grab item at start position - IF light is on - else turn return list
-    # for i in range(len(l)):
-    #     if light_is_on():
-    #         swap_item()
-    #     else:
-    #         return 'Done'
-
-    #     # (2) move to the right by one if able (not at the end of the list)
-    #     if can_move_right():
-    #         move_right()
-
-    #     else:
-    #     # move right to the beginning of the list
-    #         while can_move_left():
-    #             move_left()
-    #     # turn light off
-    #         set_light_off()
-    #     # start at (1)
-    #         continue
-    # # (3) compare: if held item is greater:
-
-    # # swap item
-    # # turn light on
-    # # move to the left one and swap with None
-    # # move left back to beginnin and start at 1
-    # # else start back at (2)
 
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
 
-    # l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1,
-    #      45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
+    l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1,
+         45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
 
-    l = [5, 4, 3, 2, 1]
+    # l = [5, 4, 3, 2, 1]
     robot = SortingRobot(l)
 
     robot.sort()
